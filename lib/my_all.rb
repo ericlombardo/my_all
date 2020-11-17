68a84e2dd81d91c8b8bd7332e590dc49fcd51d00
Eric <eric.m.lombardo@gmail.com>
2020-11-17 10:05:42 -0500

    commit tree 5be03e651d6b172609eec4cfe16982a733d3dcbe

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0   # set counter 
+  block_return_values = []    # create array for return values
+  while i < collection.length   # loop through array
+    block_return_values << yield(collection[i])   # yield item to block and push return to array
+    i += 1    # increment counter by 1
+  end
+  block_return_values.include?(false) ? false : true    # check for false in return values
+  # if block_return_values.include?(false)
+  #   false
+  # else
+  #   true
+  # end
 end
+
